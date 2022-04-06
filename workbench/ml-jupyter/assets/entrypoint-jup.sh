eee556d082bab4d870869b20454897506e37f1b0
Collonville Thomas <collonville.thomas@gmail.com>
2022-04-06 14:38:59 +0200

    finalisation workbench ml-jupyter:1.0.1

--- workbench/ml-jupyter/assets/entrypoint-jup.sh
+++ workbench/ml-jupyter/assets/entrypoint-jup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root --config=/home/$1/.jupyter/jupyter_notebook_config.py &
+jupyter-notebook --NotebookApp.notebook_dir=/home/$1/workspace --NotebookApp.base_url=/ml/$1/jupyter --ip=0.0.0.0 --port=8888 --allow-root --config=/home/$1/.jupyter/jupyter_notebook_config.py &
 
 /usr/local/sbin/entrypoint.sh shellinabox
 
